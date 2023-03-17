b79389b788038eac82257266ae147cdad5367c5c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-17 14:54:23 -0700

    printing with puts on C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
