612e77c2160ad1c7a0c9baf3a20dbf42847c3d0d
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-20 15:40:02 -0700

    possible combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int q;
+	int u;
+	int y;
+
+	for (q = 0; q <= 9; q++)
+	{
+		for (u = 1; u <= 9; u++)
+		{
+			for (y = 2; u <= 9; y++)
+			{
+				if (q < u && u < y)
+				{
+					putchar(q + '0');
+					putchar(u + '0');
+					putchar(y + '0');
+					if (q + u + y != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
