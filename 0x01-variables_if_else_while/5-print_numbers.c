9658033120d26520804e2d112d8032f6d7587d98
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-20 09:48:32 -0700

    printing base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 0; q < 10; q++)
+		printf("%d", q);
+	printf("\n");
+	return (0);
+}
