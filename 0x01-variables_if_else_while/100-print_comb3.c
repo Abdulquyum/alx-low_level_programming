cae35890313537940d5d8f6af791df2619568575
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-20 14:36:49 -0700

    different combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int q;
+	int m;
+
+	for (q = 0; q <= 9; q++)
+	{
+		for (m = 1; m <= 9; m++)
+		{
+			if (q != m && q < m)
+			{
+				putchar(q + '0');
+				putchar(m + '0');
+				if (q * m != 72)
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
