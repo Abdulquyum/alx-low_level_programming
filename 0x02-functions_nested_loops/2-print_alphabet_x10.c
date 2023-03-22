493e12d2b1705603735e30423f998cdbf33ced43
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-22 06:52:00 -0700

    prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int q;
+	char y;
+
+	for (q = 0; q <= 10; q++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+		_putchar('\n');
+	}
+}
