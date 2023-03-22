7387dc07c426804d065d1625d4847e9a0fc0e186
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-22 09:59:07 -0700

    prints sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,22 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
+		return (0);
 	}
 }
