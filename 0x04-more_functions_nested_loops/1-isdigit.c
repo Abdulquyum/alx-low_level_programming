1caa56894ba6cd7ec21608dd32c326e4678955a7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-24 06:30:03 -0700

    printing digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isdigit - checks for digit
@@ -10,12 +9,8 @@
  */
 int _isdigit(int c)
 {
-	for (c = 48; c <= 57; c++)
-	{
-		if (c == 48 && c <= 57)
-			return (1);
-		else
-			return (0);
-	}
-	return (c);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
