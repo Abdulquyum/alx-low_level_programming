125d404e85bf2f72b7320f036044e3e32a6e5512
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-24 13:52:00 -0700

    prints numerals

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numerals
+ *
+ * Return: returns 0.
+ */
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		_putchar(m + '0');
+	}
+
+		_putchar('\n');
+}
