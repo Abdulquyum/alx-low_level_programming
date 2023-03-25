c836849559b9fc636e0e61fd580c7af337030988
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-03-24 17:40:15 -0700

    Fizz_Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Alwayus 0.
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 1; q <= 100; q++)
+	{
+		if (q % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (q % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((q % 3 == 0) && (q % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", q);
+		}
+		if (q != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
