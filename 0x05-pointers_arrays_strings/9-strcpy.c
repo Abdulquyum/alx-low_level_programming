1872ab64be70d18a872c903c275ed034c1e4df41
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-11 07:24:23 -0700

    copy strings of character from source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string from source to destination
+ *
+ * @dest: parameter for destination
+ * @src: parameter for source
+ *
+ * Return: Returns destination.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int q;
+
+	for (q = 0; src[q] != '\0'; q++)
+	{
+		dest[q] = src[q];
+	}
+	return (dest);
+}
