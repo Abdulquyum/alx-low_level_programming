e2711558aaacf973c924cb2a513b4a6d724c5ec4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-12 09:11:56 -0700

    creates array of chars with initialization

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * create_array - creates array of chars with initialization
@@ -10,12 +11,23 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	for (c = 0; size[c] != '\0'; c++)
-	{
-		return (c);
-	}
+	unsigned int q;
+	char *str;
+
 	if (size == 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	q = 0;
+	while (q < size)
+	{
+		str[q] = c;
+		q++;
+	}
+	return(str);
 }
