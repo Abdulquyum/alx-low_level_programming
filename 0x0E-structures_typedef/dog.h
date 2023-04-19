99c046b255a185e0095fbb8eb97cf88ed5732b22
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-18 18:04:50 -0700

    constructed structure for my dog successfully

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - constructed to keep dog information
+ *
+ * @name: parameter for its name
+ * @age: parametr for its age
+ * @owner: parameter for its owner name
+ *
+ * Description: long description
+ */
 struct dog
 {
 	char *name;
