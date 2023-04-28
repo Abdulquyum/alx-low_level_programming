cede147f9b62e8a8a0a2e61462bc70f181a47100
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-28 16:33:24 -0700

    prints Hello, Holberton with 64-bit programassembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+extern printf
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
+
+	section .text
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+
+	ret
