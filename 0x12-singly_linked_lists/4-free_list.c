f9cc708b59c465a3b98cf996d70c508727f85ad8
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-28 15:22:04 -0700

    frees list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,11 +11,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	temp = head;
-	while (temp != NULL)
+	while (temp == NULL)
 	{
-		head = head->next;
-		free(temp->str);
-		free(temp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
