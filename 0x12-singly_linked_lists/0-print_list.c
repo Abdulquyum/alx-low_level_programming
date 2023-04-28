156dae453e2447cebf9af6f99ffbcf00980fae56
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-28 16:48:53 -0700

    prints list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,11 +17,15 @@ size_t print_list(const list_t *h)
 	while (h != 0)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
-	num_nodes++;
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num_nodes++;
 	}
 
 	return (num_nodes);
