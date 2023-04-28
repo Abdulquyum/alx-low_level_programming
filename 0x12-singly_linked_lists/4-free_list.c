02ed91d1670cc1607286f7c7665bcd6dda2c86c3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-28 15:01:30 -0700

    frees list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - fress a list_t list
+ *
+ * @head: parameter for start lis to be freedt
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (temp!= 0)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
