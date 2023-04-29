9b15030adca2640c6a0010b2e0dcf7a1bf9fb9d3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-29 03:44:35 -0700

    convert coins

---
+++ 0x08-recursion/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to change for amount of money
+ *
+ * @argc: parameter for argument count
+ * @argv: parameter for argument vector
+ *
+ * Return: Always zero.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, convert_coin = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+
+		else if (cents >= 10)
+			cents -= 10;
+
+		else if (cents >= 5)
+			cents -= 5;
+
+		else if (cents >= 2)
+			cents -= 2;
+
+		else if (cents >= 1)
+			cents -= 1;
+
+		convert_coin += 1;
+	}
+	printf("%d\n", convert_coin);
+
+	return (0);
+}
