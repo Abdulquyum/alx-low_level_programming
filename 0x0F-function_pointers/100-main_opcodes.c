04df66e85f9b6b44c0fce407244db62a8aa68b1b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-04-30 06:47:17 -0700

    prints opcodes of its main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 	int byte = atoi(argv[1]);
 
 	if (byte < 0)
 	{
 		printf("Error");
-		return (2);
+		exit(2);
 	}
 	int q;
 	unsigned char *ops = (unsigned char *) main;
