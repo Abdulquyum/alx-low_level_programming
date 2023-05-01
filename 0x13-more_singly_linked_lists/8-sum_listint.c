2a6a32f1eaa39db37ac65822c75bea2109ec85b8
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-05-01 09:25:34 -0700

    sum of all data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum all data of the list
+ *
+ * @head: parameter for start element
+ *
+ * Return: sum of list
+ */
+int sum_listint(listint_t *head) 
+{
+	int total = 0;
+
+	if (head == 0)
+		return (0);
+	else
+	{
+		while (head)
+		{
+			total += head->n;
+			head = head->next;
+		}
+	}
+
+	return (total);
+}
