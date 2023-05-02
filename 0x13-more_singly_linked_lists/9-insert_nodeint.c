f5fd5d23e463ab90a66103986433961c1411d83e
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-05-02 03:51:02 -0700

    adds node to nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,13 +19,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (count = 0; count < (idx - 1); count++)
+		while (temp)
 		{
-			while (temp != NULL)
-			{
+			for (count = 0; count < idx - 1; count++)
 				temp = temp->next;
-			}
-		}
+		}		
 	}
 	if (idx != 0 && temp == NULL)
 		return (NULL);
