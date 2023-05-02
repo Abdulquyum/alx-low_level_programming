cf5b4adf14bba708a07b4d8a783e2e10389b50a5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-05-02 16:51:52 -0700

    loop in list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lits.h"
+
+/**
+ * free_listint_safe - frees safe list
+ *
+ * @h: parameter for pointer to pointer to first element
+ *
+ * Return: size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds loop in the list
+ *
+ * @head: parameter for pointer to start
+ *
+ * Return: address of starting loop 
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+}
