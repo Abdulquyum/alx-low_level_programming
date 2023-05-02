41a727c8a3b4fbd75df3eda40fd3dc5d5c10c811
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-05-02 04:27:03 -0700

    deletes at nth position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(temp);
 		return (1);
 	}
 
@@ -36,6 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	next_N = temp->next;
 	temp->next = next_N->next;
 	free(next_N);
+	free(temp);
 
 	return (1);
 }
