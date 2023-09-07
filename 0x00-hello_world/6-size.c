c825c6b2b661b33b4d6e6345e6585b1689de8cca
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-07 16:32:03 -0700

    The journey of a thousand mile of C starts with a step

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry pointt
+/**
+ * main - Entry point
  *
  * Return: Always 0 (Success).
  */
 int main(void)
 {
-	
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
