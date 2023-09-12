86be0d46e3b8b151eb72075ac9bb38bf9fea85f3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-12 14:51:48 -0700

    Prints last digit of a set of numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ *
+ * @n: parameter passed to function
+ *
+ * Return: return last digit
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = (n % 10);
+
+	return (r);
+}
