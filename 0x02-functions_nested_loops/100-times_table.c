a16f5c35dc7c329bc53c069f0303e09cbfda9a5e
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-12 18:32:17 -0700

    accepts unknown parameter and prints its  times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_tabl - Prints n time table
+ *
+ * @n: Unknown parameter to print its table
+ *
+ * Return: Nothing.
+ */
+void print_times_table(int n)
+{
+	int a, b, result;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			result = a * b;
+			if (b == 0)
+				printf("%d", result);
+			else if (result < 10)
+				printf("   %d", result);
+			else
+				printf("  %d", result);
+			if (b < n)
+				printf(",");
+		}
+		printf("\n");
+	}
+}
+
