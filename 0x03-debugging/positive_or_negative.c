a647295431a82c75a40ac466fba53435169d9a99
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-13 07:41:26 -0700

    Check if an integer passed is positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * positive_or_negative - checks for positive or negative integer
  *
+ * @i: parameter
  * Return: Nothing.
  */
 void positive_or_negative(int i)
 {
-
-        if (i > 0)
-                printf("%d is positive\n", i);
-        else if (i == 0)
-                printf("%d is zero\n", i);
-        else
-                printf("%d is negative\n", i);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
 }
