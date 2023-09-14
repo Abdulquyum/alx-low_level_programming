1c63a24752278412f10d42e4a75daf119dd2482b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-13 17:19:55 -0700

    Added even numbers in Fibonacci number up to four million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long long int prev, cur, count, n, sum;
+
+	prev = 0, cur = 1;
+
+	for (count = 1; count <= 4000000; count ++)
+	{
+		n = cur + prev;
+		prev = cur;
+		cur = n;
+
+		if (n % 2 == 0)
+		{
+			sum += n;
+		}
+	}
+
+	printf("%llu\n", sum);
+
+	return (0);
+}
