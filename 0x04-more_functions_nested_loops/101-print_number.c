4a15b2d9be315979eff29ae7293d5b369e274902
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-15 16:31:43 -0700

    prints given number from negative up to 9999 with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,13 +9,33 @@
  */
 void print_number(int n)
 {
-	int q, count;
-
-	count = 0;
-	for (q = n; q < count; q++)
+	if (n < 0)
 	{
-	_putchar(q / 10 + '0');
-	_putchar(q % 10 + '0');
-	count++;
+		_putchar('-');
+		n = -n;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 0 && n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 1000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
