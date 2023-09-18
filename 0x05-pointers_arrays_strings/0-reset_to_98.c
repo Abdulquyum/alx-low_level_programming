2663e14572bc6d6fca0aab35fd146cf69844b16b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-18 15:03:23 -0700

    Sets the value of its pointed element to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updte point to value to 98
+ *
+ * @n: pointer parametr
+ *
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains Projects carried out using Arrays, Strings and Pointer and also duplicate inbuilt functions of string
