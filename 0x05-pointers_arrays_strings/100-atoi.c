a4f4f4a20cac3c15045debed7cbe8cf44bac1efe
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-19 15:40:00 -0700

    customised atoi function of c lang

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  _atoi - converts strings to number
+ *
+ * @s: parameter for string
+ *
+ * Return: Nothing
+ */
+int _atoi(char *s)
+{
+	int q, sign = 1, result = 0;
+
+	for (q = 0; s[q] != '\0'; q++)
+	{
+		if (s[q] == '\0')
+			break;
+		else if (s[q] == ' ')
+			continue;
+
+		if (s[q] == '-')
+			sign = -1;
+		else if (s[q] == '+')
+			sign = 1;
+
+		if (s[q] >= '0' && s[q] <= '9')
+		{
+			result = result * 10 + (s[q] - 48);
+		}
+	}
+	return (result * sign);
+}
