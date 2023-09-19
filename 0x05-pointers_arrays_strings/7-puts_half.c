f553d8e2398621eb5682f20bf4ba14f927c3ecc6
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-18 17:51:54 -0700

    Prints second half of a string in cases of odd strings lenght

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts-half - prints second half of a string
+ *
+ * @str: parameter for string
+ *
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int q, lenght = 0;
+	char string;
+
+	for (q = 0; str[q] != '\0'; q++)
+	{
+		lenght++;
+	}
+
+	if (lenght % 2 == 0)
+	{
+		for (q = lenght / 2; str[q] != '\0'; q++)
+		{
+			string = str[q];
+			_putchar(string);
+		}
+	}
+	else
+	{
+		for (q = (lenght - 1) / 2; str[q] != '\0'; q++)
+		{
+			string = str[q];
+			_putchar(string);
+		}
+	}
+	_putchar('\n');
+}
