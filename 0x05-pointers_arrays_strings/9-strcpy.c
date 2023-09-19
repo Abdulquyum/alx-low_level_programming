236e26fe18808821716f2c4a3163d61f741a0f4b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-18 18:51:50 -0700

    Hand made strcpy() that copies source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: destination of string copied
+ * @src: string to be copied
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int q;
+/*
+	for (q =0; src[q] != '\0'; q++)
+	{
+		length++;
+	}
+*/
+	for (q = 0; src[q] != '\0'; q++)
+	{
+		dest[q] = src[q];
+	}
+	dest[q] = '\0';
+
+	return (dest);
+}
