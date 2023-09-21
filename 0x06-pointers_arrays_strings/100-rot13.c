a4df79535d9a97e4e04cd1da8488c71b4f8db8a9
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-21 23:37:13 +0100

    encodes string in rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,7 +4,7 @@
  * char *rot13 - converts letters to rot13 code
  * @str: parameter for string
  *
- * return: Nothing
+ * Return: Strings
  */
 char *rot13(char *str)
 {
@@ -15,6 +15,13 @@ char *rot13(char *str)
 		if (str[q] >= 'a' && str[q] <= 'm')
 			str[q] += 13;
 		else if (str[q] >= 'n' && str[q] <= 'z')
+			str[q] -= 13;
+	}
+	for (q = 0; str[q] != '\0'; q++)
+	{
+		if (str[q] >= 'A' && str[q] <= 'M')
+			str[q] += 13;
+		else if (str[q] >= 'N' && str[q] <= 'Z')
 			str[q] -= 13;
 	}
 	return (str);
