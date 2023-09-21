3126b9b4264c0f2850c1838a2dcd0f643b060376
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-21 22:21:16 +0100

    compares two strings, work exactly like in_built strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,10 +20,9 @@ int _strcmp(char *s1, char *s2)
 	{
 		len2++;
 	}
-
-	if (len1 < len2)
+	if (s1[q] < s2[2] && len1 < len2)
 		return (15);
-	else if (len1 > len2)
+	else if (s1[q] > s2[q] && len1 > len2)
 		return (-15);
 	else
 		return (0);
