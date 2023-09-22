7fb27ce5141d2d88bf578069582b8d1c24ac1751
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-22 01:37:26 +0100

    Reverses the elements inside an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: array of int
+ * @n: number of elements to reverse
+ *
+ * Return: rest of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int q, save;
+
+	for (q = 0; q < n / 2; q++)
+	{
+		save = a[q];
+		a[q] = a[n -q -1];
+		a[n - q - 1] = save;
+	}
+}
+
