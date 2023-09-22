d4918f013a6948ec64524ad87c5bba82c2b4ebaf
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-22 03:44:42 +0100

    Prints interger with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -26,7 +26,7 @@ void print_number(int n)
 	else if (n < 1000)
 	{
 		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10+ '0');
+		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
 	else if (n > 999)
