6e18727f2346e0e3ca5419bee654f277c8c37a6d
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-25 16:26:12 -0700

    fills memory with a constant byte, custom memset function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: value to be set
+ * @to: value to set to
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	int q;
+
+	if (to == 0)
+		return;
+
+	for (q = 0; to[q] != '\0'; q++)
+	{
+		*s[q] = to[q];
+	}
+}
