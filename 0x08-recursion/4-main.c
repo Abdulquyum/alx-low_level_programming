b7b28e27997759ad510646537d5679a6442b5b88
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-09-29 12:20:08 +0100

    prints x raise to power y

--- 0x08-recursion/4-main.c
+++ 0x08-recursion/4-main.c
@@ -12,7 +12,7 @@ int main(void)
 
 	r = _pow_recursion(1, 10);
 	printf("%d\n", r);
-	r = _pow_recursion(1024, 1);
+	r = _pow_recursion(1024, 0);
 	printf("%d\n", r);
 	r = _pow_recursion(2, 16);
 	printf("%d\n", r);
