74b13298a60b220c11eb80453af75e99fab2e2d7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-03 01:29:02 +0100

    adds several numbers passed as argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always )
+ */
+int main(int argc, char *argv[])
+{
+	int q, result = 0;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+
+	for (q = 2; q <= argc; q++)
+	{
+		if (*argv[q] >= 'a' && *argv[q] <= 'z')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			result += atoi(argv[q]);
+	}
+
+	printf("%d\n", result);
+	
+	return (0);
+}
+
