15726f7cdfe4c36f8ca15254fcd00f93368ac234
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-03 02:12:43 +0100

    prints minimum number of coins to make change of an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
