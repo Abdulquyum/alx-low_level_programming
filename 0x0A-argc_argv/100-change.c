6c44754167ccdec7322c6a258b8c356cd3f6c509
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-03 03:21:53 +0100

    prints minimum number of coins to make change of an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -10,11 +11,33 @@
  */
 int main(int argc, char *argv[])
 {
+	int total;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	return (0);
-}
+	total = atoi(argv[1]);
+
+	if (total < 0) {
+		printf("0\n");
+		return 0;
+	}
+
+
+	int i;
+	int coins[] = {25, 10, 5, 2, 1};
+	int coin_count = 0;
+
+	for (i = 0; i < sizeof(coins) / sizeof(coins[0]); i++)
+	{
+		int result = total / coins[i];
+		coin_count += result;
+		total -= result * coins[i];
+	}
+
+	printf("%d\n", coin_count);
+
+	return 0;
+	}
