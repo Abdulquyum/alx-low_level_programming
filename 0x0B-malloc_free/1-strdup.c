532307b260670348df829d0d6f3e77bdcbfa4358
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-04 21:10:06 +0100

    string duplicate custom function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string
+ *
+ * @str: parameter for string
+ *
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int q, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	 for (q = 0; str[q] != '\0'; q++)
+		 len++;
+
+	ptr = malloc(len * sizeof(char) + 1);
+
+	for (q = 0; str[q] != '\0'; q++)
+	{
+		if (ptr == NULL)
+			return (NULL);
+
+		ptr[q] = str[q];
+	}
+	ptr[q] = '\0';
+
+	return (ptr);
+}
