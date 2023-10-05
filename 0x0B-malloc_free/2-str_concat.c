0ee3481a9e41c98b1de3e31500ae039a3ecb3c90
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-05 23:46:51 +0100

    custom string concatenate with dynamic allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,18 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int q, i, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (q = 0; s1[q] != '\0'; q++)
 		len1++;
 
 	for (i = 0; s2[i] != '\0'; i++)
 		len2++;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
 
 	ptr = malloc(len1 * sizeof(char) + len2 * sizeof(char) + 1);
 
