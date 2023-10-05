2556b0da33f184ed68b9450ee38442085234af39
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-05 02:58:21 +0100

    custom string concatenate with dynamic allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints two dimensional array
+ *
+ * @width: row represenation
+ * @height: column representation
+ *
+ * Return: pointer to 2 dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int q, i;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	ptr = malloc(height * sizeof(int *));
+
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (q= 0; q < height; q++)
+	{
+		ptr[q] = malloc(width * sizeof(int *));
+	}
+	for (i = 0; i < width; i++)
+	{
+		ptr[q][i] = 0;
+	}
+	
+	return (ptr);
+}
