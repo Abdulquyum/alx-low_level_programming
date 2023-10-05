2584fd73da8593cd290230130b99410111498bb4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-05 04:25:19 +0100

    deallocate previously allocated memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free previously created array
+ *
+ * @grid - 2 dimensional array
+ * @height - rows or height of the array
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int q;
+
+	for (q = 0; q < height; q++)
+	{
+			free(grid[q]);
+	}
+	free(grid);
+}
