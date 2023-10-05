03bea27d0a6d766c9d8c5232d2a26318996494f1
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-05 18:41:12 +0100

    custom argstostr function, conacatenates argv arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * argstostr - concatenates all command line arguments
@@ -13,30 +12,40 @@
 char *argstostr(int ac, char **av)
 {
 	char *ptr;
-	int q, i, str_len = 0;
-	int iterate = 0, arg_len = 0;
+	int q, i, av_len = 0;
+	int iterate = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (q = 0; q < ac; q++)
 	{
-		str_len += strlen(argv[q]) + 1;
+		av_len += _strlen(av[q]) + 1;
+	}
 
-	ptr = malloc(str_len);
+	ptr = malloc(av_len);
 
 	if (ptr == NULL)
 		return (NULL);
 
 	for (q = 0; i < ac; q++)
 	{
-			int arg_len = strlen(av[q]);
-			strcpy(ptr + iterate, av[i]);
-			iterate += arg_len;
-			ptr[iterate++] = '\n';
-
+		for (i = 0; av[q][i] != '\0'; i++)
+		{
+			ptr[iterate++] = av[q][i];
+		}
+		ptr[iterate++] = '\n';
 	}
-	ptr[iterate] = '\0';
-	return (ptr);
+	ptr[iterate - 1] = '\0';
 
+	return (ptr);
 }
+
+int _strlen(char *str)
+{
+	if (*str == '\0')
+		return (0);
+
+	return (1 + _strlen(str + 1));
+}
+			
