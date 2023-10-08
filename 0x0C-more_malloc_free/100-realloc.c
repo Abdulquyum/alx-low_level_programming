749051d086be64a9c50bd4c2982bca539d699f4c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-08 14:48:38 +0100

    custom realloc function, allocate memory block including previous and formal memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int min_byte;
 
 	if (ptr == NULL)
-		new_ptr = malloc(new_size);
+		return (malloc(new_size));
 
 	if (new_size == 0 && ptr != NULL)
 	{
