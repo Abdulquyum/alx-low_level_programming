841bdcdcfff048353e6ff924b462466035cd3eeb
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-11 01:17:39 +0100

    defined struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure created for dog
+ *
+ * @name: member, name of dog
+ * @age: dog's age
+ * @owner: dog owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
