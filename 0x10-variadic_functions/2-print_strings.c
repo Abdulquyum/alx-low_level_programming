4d57f0ae350c9c234665e68f509781839215892e
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-13 02:38:43 +0100

    Prints strings then a comma

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints string of character
+ *
+ * @separator: comma to seperate string
+ * @n: number of argument to be passed
+ *
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int q;
+	char *str;
+
+	va_list strings;
+
+	va_start(strings, n);
+
+	for (q = 0; q < n; q++)
+	{
+		str = va_arg(strings, char *);
+
+		if (separator != NULL)
+		{
+			if (str != NULL)
+			{
+				if (q < n - 1)
+				{
+					printf("%s%s", str, separator);
+				}
+				else
+					printf("%s", str);
+			}
+			else
+				printf("%s", "nil");
+		}
+		else
+			return;
+	}
+
+	printf("\n");
+
+	va_end(strings);
+}
