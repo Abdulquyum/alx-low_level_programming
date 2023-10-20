79840f0e59b4721ae9c715a5b6e2e6db8fe6b619
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-20 11:22:07 +0100

    prints all the elements of a list_t list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,17 @@
 extern printf
 
 section .data
-greet: db "Hello, Holberton", 0
-fmt: db "%s", 10, 0
+        greet db "Hello, Holberton", 0
+        fmt db "%s", 10, 0
 
 section .text
-global main
+	global main
 
 main:
-push rbp
-
-mov rdi, fmt
-mov rsi, greet
-mov rax, 0
-mov rax, 0
-call printf
-pop rbp
-
-ret
+	push rbp
+	mov rdi, fmt
+	mov rsi, greet
+	xor rax, rax
+	call printf
+	ret
+	pop rbp
