1aea2ec523976d6c05176d30e07ea333bbf606c5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-20 04:29:19 +0100

    A story printed before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 
-void print_story()
+void __attribute__((constructor)) print_story(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
