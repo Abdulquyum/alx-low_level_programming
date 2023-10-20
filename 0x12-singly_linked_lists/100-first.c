3baf0ba1cd633ee343dd21b13dd9fb2fcd27ba2c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-20 09:53:17 +0100

    prints before the main function without calling it in the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_story - prints story of tortoise and hare before main
+ *
+ * Return: Nothing
+ */
 void __attribute__((constructor)) print_story(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
