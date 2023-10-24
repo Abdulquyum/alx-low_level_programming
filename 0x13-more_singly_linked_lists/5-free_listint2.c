e5a9e3838614e191d1d74bfcc45ebcfc6b5893fb
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-24 23:11:26 +0100

    free lsitint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - fres a listint_t list
+ *
+ * @head: pointer to pointer to list
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = *head;
+		temp = temp->next;
+
+		free(*head);
+	}
+}
