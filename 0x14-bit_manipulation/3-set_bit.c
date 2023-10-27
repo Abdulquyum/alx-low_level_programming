503689dfc57da2cb6c3c4a9a7b996cf5875a2719
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-27 03:34:38 +0100

    set value of bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of a bit at nth position
+ *
+ * @n: argument
+ * @index: nth position
+ *
+ * Return: value of the bit
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask, result;
+
+	if (index >= *n)
+		return (-1);
+
+	mask = 1 << index;
+
+	result = *n | mask;
+
+	if (result > 0)
+		return (1);
+	else
+		return (0);
+}
