a81e3ef3478796936602f2a4201aa220f0fc5e35
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-31 23:53:12 +0100

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,8 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int writetxt;
 
-	fd = open(filename, O_CREAT | O_RDWR);
+	if (text_content == NULL)
+		fd = open(filename, O_CREAT | O_RDWR);
 
 	if (filename == NULL)
 		return (-1);
@@ -25,9 +26,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (writetxt < 0)
 		return (-1);
-
-	if (text_content == NULL)
-		return (0);
 
 	close(fd);
 
