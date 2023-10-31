cfd45333a76e0ca89675067032b1d30f4502a6c5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-31 04:19:32 +0100

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - prgram to reads text file
+ *
+ * @filename: the of file to be read
+ * @letters: number of etters to read and print
+ *
+ * Return: number of letters else 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buff;
+	ssize_t	readtxt, writetxt;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	buff = malloc(sizeof(letters) + 1);
+
+	readtxt = read(fd, buffer, letters);
+
+	close(fd);
+
+	if (readtxt == -1)
+		free(buffer);
+
+	bufer[readtxt] = '\0';
+
+	writetxt = write(STDOUT_FILENO, buffer, readtxt);
+
+	free(buffer);
+
+	if (writetxt == -1)
+		return (0);
+
+	if (writetxt != readtxt)
+		return (0);
+
+	return (readtxt);
+}
