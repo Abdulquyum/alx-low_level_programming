05225757381d7e2bdbb594d738c3ca696115bd58
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-11-03 10:22:37 +0100

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t writetxt;
 
 	if (filename != NULL)
-		truncate(filename, 0);
+		open(filename, O_TRUNC);
 
 	if (filename == NULL)
 		return (-1);
