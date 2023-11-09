2b058b9906f7c514fbea80ad4e12870615f9fe40
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-11-09 09:07:12 +0100

    creates a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,10 +12,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL)
+	if (filename == NULL || *filename == '\0')
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content == NULL || *text_content == '\0')
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
