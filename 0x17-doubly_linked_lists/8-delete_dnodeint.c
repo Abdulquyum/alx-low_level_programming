e0ed40b5b56eb2d4acdce22b00ee7fb4fd374df8
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-15 12:06:02 +0100

    delete node at index position

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 	q = 0;
-	while (q < index)
+	while (q < index && temp != NULL)
 	{
 		if (temp->next == NULL)
 			return (-1);
